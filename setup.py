d1481c358372042e695876035143b07832c39306
John Soklaski <jsokla@microsoft.com>
2017-07-19 01:14:18 -0600

    Pass -std=c++14 and -lstdc++fs when compiling under Linux for filesystem support

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env/python
 
 import os
-import sys
+import platform
 
 from setuptools import setup
 from setuptools.extension import Extension
 
 source_files = [os.path.join("src", x) for x in os.listdir("src") if x.lower().endswith(".cpp")]
+
+extra_compile_args = []
+extra_link_args = []
+
+if platform.system() == "Linux":
+      extra_compile_args = ["-std=c++14"]
+      extra_link_args = ["-lstdc++fs"]
 
 setup(name="ccscript",
       version="1.338",
@@ -15,6 +22,8 @@ setup(name="ccscript",
       ext_modules=[
           Extension("ccscript",
                     source_files,
-                    language="c++"
+                    language="c++",
+                    extra_compile_args=extra_compile_args,
+                    extra_link_args=extra_link_args
                     )
       ])
